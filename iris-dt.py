b45c0003a46ba2f495387d7266387e29bde2d565
Patel Rahul <rahulpatel16092005@gmail.com>
2026-03-27 16:57:44 +0530

    dagshub setup

--- iris-dt.py
+++ iris-dt.py
@@ -17,7 +17,7 @@ y = load_iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-max_depth = 5
+max_depth = 3
 
 mlflow.set_experiment("iris-dt")
 
